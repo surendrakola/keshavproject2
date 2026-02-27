0989afc9264513a422cc1b7bf924ae056ca9b1d5
SURENDRA KOLA <surendrakola08@gmail.com>
2026-02-27 16:04:59 +0530

    Set up Nunjucks for template rendering

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Internship Task 2 Loaded Successfully 🚀");
+import nunjucks from 'nunjucks'
+
+// Configure Nunjucks
+nunjucks.configure('src/templates', {
+  autoescape: true
+})
+
+// Render template
+const output = nunjucks.render('index.njk', {
+  title: "Internship Task 2"
+})
+
+// Inject into page
+document.body.innerHTML = output
